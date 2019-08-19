9358e955919330eddd7fc4ae1c001243b0a96d0f
Sara McCombs <thesaramccombs@gmail.com>
2019-08-19 00:00:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -39,7 +39,7 @@ end
 # method named 'printer'
 # method will output the results of the 'batch_badge_creator' and 'assign_rooms' methods
 
-def printer
+def printer(speakers)
   batch_badge_creator(speakers)
   assign_rooms(speakers)
 end
