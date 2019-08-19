a73a5b7912d4fc8e1ea775e7f60828fb44ef5db7
Sara McCombs <thesaramccombs@gmail.com>
2019-08-19 00:18:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -40,6 +40,8 @@ end
 # method will output the results of the 'batch_badge_creator' and 'assign_rooms' methods
 
 def printer(speakers)
-  
+  batch_badge_creator(speakers).each do |badge|
+    puts badge
+  end
  
 end
